37bd6ae0984d0ba74748d363912058e816b69a60
Kasper Ingdahl Andkjær <sswkasper@hotmail.com>
2014-04-24 13:56:08 +0200

    PlayerController CustomEditor
    Made editor for playerController. Can now make a list of spawnable mobs
    and bind keys accordingly.

---
+++ Assets/Editor/PlayerControlEditor.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Linq;
+
+[CustomEditor(typeof(PlayerController))]
+public class PlayerControlEditor : Editor
+{
+	
+	public override void OnInspectorGUI()
+	{
+		PlayerController plCtl = (PlayerController)target;
+		
+		var list = plCtl.thingsThatCanBeSpawned;
+		
+		GUILayout.BeginHorizontal();
+		if (GUILayout.Button("Add new mobtype")) {
+			// if there is no goal parameter with the same name
+			list.Add(new PlayerController.SpawnerThing());
+			/*if (!list.Exists(gp => gp.parameterName == n)) {
+				// create new goal parameter, initialize all the stuff from StatusParameter
+
+				
+			}*/
+			
+		}
+		
+		if (GUILayout.Button("Clear all mobtypes")) {
+			plCtl.thingsThatCanBeSpawned.Clear();
+			
+		}
+
+		GUILayout.EndHorizontal();
+		
+		PlayerController.SpawnerThing deletedParameter = null;
+		
+		foreach (var pt in plCtl.thingsThatCanBeSpawned) {
+			
+			// param name
+			//pt.parameterName = EditorGUILayout.TextField("Param name", pt.parameterName);
+			
+			//EditorGUILayout.LabelField("Parameter " + gp.parameterName);
+			
+			GUILayout.BeginHorizontal();
+			EditorGUILayout.ObjectField("Spawnable Ref", (Object)pt.spawnableRef, typeof(GameObject), false);
+			GUILayout.EndHorizontal();
+			
+			GUILayout.BeginHorizontal();
+			pt.spawnKey = (KeyCode)EditorGUILayout.EnumPopup("Main keybind for spawn", pt.spawnKey);
+			pt.spawnKey2 = (KeyCode)EditorGUILayout.EnumPopup("Alt keybind for spawn", pt.spawnKey2);
+			// show value
+			/*
+			if (gp.statusParameter.parameterType == ParameterTypes.Float) {
+				EditorGUILayout.LabelField("Value: " + (float)gp.statusParameter.Value);
+
+			} else if (gp.statusParameter.parameterType == ParameterTypes.Bool) {
+				EditorGUILayout.LabelField("Value: " + (bool)gp.statusParameter.Value);
+
+			}
+			*/
+			
+			// multiplier for floats
+		/*	if (pt.statusParameter.parameterType == ParameterTypes.Float) {
+				pt.multiplier = EditorGUILayout.FloatField("Multiplier: ", pt.multiplier);
+			}
+		*/	
+			GUILayout.EndHorizontal();
+			
+			GUILayout.BeginHorizontal();
+			// param type
+			//pt.statusParameter.parameterType = (ParameterTypes)EditorGUILayout.EnumPopup("Type", pt.statusParameter.parameterType);
+			
+			if (GUILayout.Button("Delete parameter")) {
+				deletedParameter = pt;
+				
+			}
+			
+			GUILayout.EndHorizontal();
+			
+		}
+		
+		if (deletedParameter != null) {
+			plCtl.thingsThatCanBeSpawned.Remove(deletedParameter);
+		}
+		
+		if (GUI.changed) {
+			EditorUtility.SetDirty(plCtl);
+		}
+	}
+	
+}
---
+++ Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+	public List<SpawnerThing> thingsThatCanBeSpawned = new List<SpawnerThing>();
+
+	[System.Serializable]
+	public class SpawnerThing {
+			//The object to spawn when the appropriate input is found.
+		public GameObject spawnableRef;
+			//The input to spawn from.
+		public KeyCode spawnKey;
+		public KeyCode spawnKey2;
+
+			//Constructor for eventual things to be defined by add new spawnerthing. Called in PlayerControlEditor on when adding to list.
+		public SpawnerThing(){
+
+		}
+	}
+
+	void update(){
+		foreach(SpawnerThing thingToSpawn in thingsThatCanBeSpawned){
+			if(	Input.GetKey(thingToSpawn.spawnKey)
+			   		||
+			   	Input.GetKey(thingToSpawn.spawnKey2)
+			){
+				Instantiate (thingToSpawn.spawnableRef, transform.position, Quaternion.identity);
+			}
+		}
+	}
+}
+
