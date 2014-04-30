b56c519b43fc15324f46e16f597bdb7d755656e5
Kasper Ingdahl Andkjær <sswkasper@hotmail.com>
2014-04-30 14:00:37 +0200

    Final comments

--- Assets/Game/Scripts/TriggerFixToBeOnParent.cs
+++ Assets/Game/Scripts/TriggerFixToBeOnParent.cs
@@ -2,19 +2,21 @@
 using System.Collections;
 
 public class TriggerFixToBeOnParent : MonoBehaviour {
-	
+		//Making two custom events
 	public delegate void StartColliding(Collider collider);
 	public event StartColliding StartCollideWithOther;
-
 	public delegate void StopColliding(Collider collider);
 	public event StopColliding StopCollideWithOther;
 
+
+		//When OntriggerEnter is called, activate the StartCollideWithOther-event
 	void OnTriggerEnter(Collider collider){
 		StartCollideWithOther(collider);
 	}
 
+
+		//When OntriggerExit is called, activate the StopCollideWithOther-event
 	void OnTriggerExit(Collider collider){
 		StopCollideWithOther(collider);
 	}
-
 }
